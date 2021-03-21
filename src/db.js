723726a66045baed98529d9398edf120aad3ee24
athiban2001 <captainathiban@gmail.com>
2021-03-21 23:10:22 +0530

    Database Connection

--- src/db.js
+++ src/db.js
@@ -1,11 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-	user: process.env.DB_USERNAME,
-	password: process.env.DB_PASSWORD,
-	host: process.env.DB_HOST,
-	port: 5432,
-	database: process.env.DB_NAME,
+	connectionString: process.env.DATABASE_URL,
 });
 
 const query = async (queryString, parameters) => {
