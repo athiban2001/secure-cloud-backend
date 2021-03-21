9b0bb7ab8c06d7445e505246718303d250f497ba
athiban2001 <captainathiban@gmail.com>
2021-03-21 23:20:31 +0530

    Database Connection

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/admin", adminRouter);
-app.use("/api/user", userRouter);
-app.use("/api/manager", managerRouter);
+// app.use("/api/user", userRouter);
+// app.use("/api/manager", managerRouter);
 
 app.listen(process.env.PORT, () => {
 	console.log("Server is running on http://localhost:" + process.env.PORT);
