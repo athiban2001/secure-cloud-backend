45e0613395ab40ba85d407fa1be34a44b9c28723
athiban2001 <captainathiban@gmail.com>
2021-03-28 23:32:23 +0530

    40% implementation bug fixes

--- src/manager/routes.js
+++ src/manager/routes.js
@@ -71,7 +71,7 @@ managerRouter.get("/requests", jwtAuth("MANAGER"), async (req, res) => {
 		response,
 		error,
 	} = await db.query(
-		"SELECT r.id,r.user_id,r.group_id,r.xi,r.joining,r.ok,(SELECT name FROM users WHERE id=r.user_id) as user_name,(SELECT email FROM users WHERE id=r.user_id) as email,(SELECT name FROM groups WHERE id=r.group_id) AS group_name FROM requests r WHERE r.group_id=1 AND r.ok IS NULL",
+		"SELECT r.id,r.user_id,r.group_id,r.xi,r.joining,r.ok,(SELECT name FROM users WHERE id=r.user_id) as user_name,(SELECT email FROM users WHERE id=r.user_id) as email,(SELECT name FROM groups WHERE id=r.group_id) AS group_name FROM requests r WHERE r.group_id=$1 AND r.ok IS NULL",
 		[groupId]
 	);
 	if (error) {
